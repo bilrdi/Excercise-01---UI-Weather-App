aaee15ce1c3fb9a37871176ab1e85aa8f175dda9
bilrdi <billiumardaeli@gmail.com>
2022-08-30 08:42:33 +0700

    Initial Commit

---
+++ front end weather app sample/front_end_weather_app_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  front_end_weather_app_sampleApp.swift
+//  front end weather app sample
+//
+//  Created by Billi Umar Daeli on 30/08/22.
+//
+
+import SwiftUI
+
+@main
+struct front_end_weather_app_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
